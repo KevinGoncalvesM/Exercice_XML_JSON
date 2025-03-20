3a5d9834952a9a06ca6968d77deea257299046a9
KevinGoncalvesM <goncalves.jk@outlook.fr>
2025-03-20 16:31:37 +0100

    Amélioration du code : plus efficace pour les grandes bases de données

--- code.py
+++ code.py
@@ -5,8 +5,7 @@ import sqlite3
 import datetime
 import base64
 
-
-#Fonction qui permet de notifier en logs que plusisurs JSON on le même nom
+#Fonction qui permet de notifier en logs que plusisurs JSON ont le même nom
 def warning_dup(fichiers):
     set_fichiers = set() # Permet d'éviter tout doublon
     doublons = set()
@@ -75,6 +74,34 @@ def request_db(path_db, file_db, table_name) :
 
     except:
         logging.error(f"Erreur lors de l'exécution de la requête SQL, la table {table_name} n'existe pas ")
+
+    finally:
+        connection.close()
+
+
+#Ici la requête est ciblée
+def request_db_opti(path_db, file_db, table_name,attribut, valeur) :
+    db_path = os.path.join(path_db, file_db[0]) #connexion à la base de données
+
+    try:
+        connection = sqlite3.connect(db_path)
+        cursor = connection.cursor()
+    except:
+        logging.error(f"Erreur de connexion à la base de données")
+        exit(1)
+
+    try:
+        #ici on fait une requete sur la table que l'on souhaite
+        query = f"SELECT * FROM {table_name} WHERE {attribut} = ?;"
+        cursor.execute(query, (valeur,)) #on passe valeur en tuple pour eviter une erreur de requête
+
+        lignes = cursor.fetchall()
+
+        return lignes
+
+    except:
+        logging.error(f"Erreur lors de l'exécution de la requête SQL, la table {table_name} n'existe pas ")
+        return None
 
     finally:
         connection.close()
@@ -157,6 +184,34 @@ def comparaison_ligne(data_json, data_msg_db, dict_contact_db, fichier):
     logging.info((f"Le fichier {fichier} est définitvement correct"))
     return True
 
+
+def comparaison_ligne_opti(data_json, data_msg_db, data_contact_db, fichier): #Ici on compare deux listes
+
+    is_false = 0
+
+    if data_json["timestamp"] != data_msg_db[0][1]:
+        logging.error(f"{fichier} : Le timestamp {data_json['timestamp']} ne correspond pas au timestamp de la bdd : {data_msg_db[0][1]}")
+        is_false += 1
+
+    if data_json["direction"] != data_msg_db[0][2]:
+        logging.error(f"{fichier} : La direction {data_json['direction']} ne correspond pas à la direction de la bdd : {data_msg_db[0][2]}")
+        is_false += 1
+
+    encoded_msg = base64.b64encode(data_msg_db[0][3].encode("utf-8")).decode("utf-8")
+
+    if data_json["content"] != encoded_msg:
+        logging.error(f"{fichier} : Le contenu {data_json['content']} ne correspond pas au contenu de la BDD : {encoded_msg}")
+        is_false += 1
+
+    if data_json["contact"] != data_contact_db[0][1]:
+        logging.error(f"{fichier} : Le nom {data_json['contact']} ne correspond pas au contact de la bdd : {data_contact_db[0][4]}")
+        is_false += 1
+
+    if is_false > 0:
+        return False
+
+    logging.info((f"Le fichier {fichier} est définitvement correct"))
+    return True
 
 
 def test1(path) :
@@ -313,6 +368,53 @@ def test4(path_json, path_db):
                 os.mkdir(path_issue_json)
             os.rename(file_path_json, os.path.join(path_issue_json, file_json))
 
+# Il s'agit d'une amélioration du test 4 qui ne charge pas toute la base de données afin que le code puisse être utilisable
+# même pour une grande base de données
+# Ici on effectue une requête WHERE pour récupérer l'id du message et on compare directement cette valeur avec la nouvelle
+# Ici on oublie le test 3 car la base de données est trop grande
+def test_3_4_opti(path_json, path_db):
+    logging.info("\nDébut du test 3b - Vérification de la mise en relation des données ")
+
+    #Vérification des dossier
+    #IN
+    if not os.path.exists(path_db):
+        logging.error(f"Le dossier {path_db} n'existe pas")
+        exit(1)
+    file_db = os.listdir(path_db)
+
+    #OUT
+    files_json = os.listdir(path_json) #lecture des fichiers après le test 1
+    if not files_json:
+        logging.error(f"Le dossier {path_json} est vide")
+        exit(1)
+
+    for file_json in files_json:
+
+        file_path_json = os.path.join(path_json, file_json)
+        path_issue_json = "../data/out_pb"
+
+        with open(file_path_json, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        id = data["id"]
+        name = data["contact"]
+
+        #Ici les requêtes sons ciblées
+        requet_messages_db = request_db_opti(path_db, file_db, "messages", "id", id)
+        requete_contact_db = request_db_opti(path_db, file_db, "contact", "name", name)
+
+        if requet_messages_db is None or requet_messages_db == [] or requete_contact_db is None or requete_contact_db == []: #ici on regarde si la requete est possible et si elle ne renvoie pas rien
+            if not os.path.exists(path_issue_json):
+                os.mkdir(path_issue_json)
+            os.rename(file_path_json, os.path.join(path_issue_json, file_json))
+        else:
+            if not comparaison_ligne_opti(data, requet_messages_db, requete_contact_db, file_json):
+                if not os.path.exists(path_issue_json):
+                    os.mkdir(path_issue_json)
+                os.rename(file_path_json, os.path.join(path_issue_json, file_json))
+
+
+
 def resultat(chemin_out, chemin_out_pb):
     if not os.path.exists(chemin_out):
         logging.error(f"Le dossier {chemin_out} n'existe pas")
@@ -349,6 +451,7 @@ path_in = "../data/in"
 
 test1(path_out)
 test2(path_out)
-test3(path_out, path_in)
-test4(path_out, path_in)
+#test3(path_out, path_in)
+#test4(path_out, path_in)
+test_3_4_opti(path_out, path_in)
 resultat(path_out, path_out_pb)
